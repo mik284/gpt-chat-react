05bb5327c04f978b1a3e117154e5ccdfa0e938ae
mik284 <mikeykamau222@gmail.com>
2023-02-24 11:30:30 +0300

    creating a prompt value

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import { useState } from "react";
 import "./App.css";
 import lens from "./assets/lens.png";
 
 function App() {
+  const [prompt, updatePrompt] = useState(undefined)
   return (
     <div className="app">
       <div className="app-container">
@@ -11,6 +13,8 @@ function App() {
             placeholder="Ask me Anything ..."
             style={{ backgroundImage: `url(${lens})` }}
             className="spotlight__input"
+            autoFocus
+            onChange={(e)=> updatePrompt(e.target.value)}
           />
           <div className="spotlight__answer">
             <p>Kenya is a semi-arid country and has warm and sunny climate!</p>
