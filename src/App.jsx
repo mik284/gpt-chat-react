54736b15934be20e6185448d156ff8c4f63b8ecb
mik284 <mikeykamau222@gmail.com>
2023-02-24 11:21:30 +0300

    updated app component added input

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
- 
-  return <h2>Hello world!</h2>;
-
+  return (
+    <div className="app">
+      <div className="app-container">
+        <div className="spotlight__wrapper">
+          <input
+            type="text"
+            placeholder="Ask me Anything ..."
+            style={{ backgroundImage: `url(${lens})` }}
+            className="spotlight__input"
+          />
+          <div className="spotlight__answer">
+            Kenya is a semi-arid country and has warm and sunny climate!
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
